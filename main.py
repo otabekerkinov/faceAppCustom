dac828e39f8b800be85550d5f2b1c171b9308097
Ahmed Elkholy <ahmed.elkholy@olx.pl>
2023-12-26 17:28:33 +0100

    added confusion matrix and age histogram

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from torchvision import transforms
 import torchvision.models as models
 import torch.nn as nn
 from PIL import Image
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 
 # Load the trained age detection model
@@ -29,7 +31,31 @@ age_transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
+def map_age_to_group(age):
+    if age < 10:
+        return '1-9'
+    elif age < 20:
+        return '10-19'
+    elif age < 30:
+        return '20-29'
+    elif age < 40:
+        return '30-39'
+    elif age < 50:
+        return '40-49'
+    elif age < 60:
+        return '50-59'
+    elif age < 70:
+        return '60-69'
+    elif age < 80:
+        return '70-79'
+    elif age >= 80:
+        return '80+'
 
+def predict_age_group(face_image):
+    predicted_age = predict_age(face_image)
+    age_group = map_age_to_group(predicted_age)
+    return age_group
+    
 def predict_age(face_image):
     """
     Predict the age of a face.
@@ -42,18 +68,57 @@ def predict_age(face_image):
 
 # process_dataset processes all images of a given directory and plots the cumulative data
 def process_dataset(dataset_directory):
-    all_face_sizes = []
+    actual_age_groups = []
+    predicted_age_groups = []
 
-    for filename in os.listdir(dataset_directory):
-        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):  # Checking for image files
-            file_path = os.path.join(dataset_directory, filename)
-            image = cv2.imread(file_path)
-            if image is not None:
-                _, _, face_sizes = detect_faces(image)
-                all_face_sizes.extend(face_sizes)  # Accumulate face sizes
+    # Get the actual age labels for the dataset
+    actual_age_labels = get_actual_age_labels(dataset_directory)
 
-    # After processing all images, plot the cumulative data
-    plot_face_data(all_face_sizes)
+    # Iterate over each image path and its corresponding actual age in the dataset
+    for image_path, actual_age in actual_age_labels.items():
+        image = cv2.imread(image_path)
+        if image is not None:
+            # The detect_faces function now returns predicted ages instead of face sizes
+            _, _, predicted_ages = detect_faces(image)
+            # For each detected face, match the predicted age group with the actual age group
+            for predicted_age in predicted_ages:
+                predicted_age_group = map_age_to_group(predicted_age)
+                actual_age_group = map_age_to_group(actual_age)
+                predicted_age_groups.append(predicted_age_group)
+                actual_age_groups.append(actual_age_group)
+
+    # At this point, actual_age_groups and predicted_age_groups should have the same length
+    # Compute the confusion matrix and plot it
+    compute_and_plot_confusion_matrix(actual_age_groups, predicted_age_groups)
+
+
+def compute_and_plot_confusion_matrix(actual_age_groups, predicted_age_groups):
+    age_groups = ['1-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+']
+    conf_matrix = confusion_matrix(actual_age_groups, predicted_age_groups, labels=age_groups)
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(conf_matrix, annot=True, fmt='d', xticklabels=age_groups, yticklabels=age_groups)
+    plt.xlabel('Predicted Age Group')
+    plt.ylabel('Actual Age Group')
+    plt.title('Age Group Confusion Matrix')
+    plt.show()
+
+
+def get_actual_age_labels(dataset_directory):
+    actual_age_labels = {}
+    # Iterate through each folder in the base directory
+    for folder_name in os.listdir(dataset_directory):
+        folder_path = os.path.join(dataset_directory, folder_name)
+        # Check if it's a directory and the folder name is a number
+        if os.path.isdir(folder_path) and folder_name.isdigit():
+            # Convert folder name to integer age
+            age = int(folder_name.lstrip("0"))  # Remove leading zeros
+            # Append the age label for each image file in the folder
+            for file_name in os.listdir(folder_path):
+                if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+                    file_path = os.path.join(folder_path, file_name)
+                    actual_age_labels[file_path] = age
+    return actual_age_labels
+
 
 
 # detect_faces takes a cv2 image object and uses pretrained model to detect faces and draws a rectangle around it
@@ -62,51 +127,44 @@ def detect_faces(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 
-    face_sizes = []
+    predicted_ages = []  # List to store predicted ages
     for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        face_sizes.append(w*h)  # Adding face size (area) for EDA
+       # Extract each face
+       face_image = image[y:y+h, x:x+w]
+       face_image_pil = Image.fromarray(face_image)  # Convert to PIL image
+       # Predict age
+       predicted_age = predict_age(face_image_pil)
+       predicted_ages.append(predicted_age)  # Append predicted age to the list
+       # Draw rectangle and put text (age)
+       cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+       cv2.putText(image, f'Age: {int(predicted_age)}', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+    return image, faces, predicted_ages
 
-    for (x, y, w, h) in faces:
-        # Extract each face
-        face_image = image[y:y+h, x:x+w]
-        face_image_pil = Image.fromarray(face_image)  # Convert to PIL image
-
-        # Predict age
-        predicted_age = predict_age(face_image_pil)
-
-        # Draw rectangle and put text (age)
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(image, f'Age: {int(predicted_age)}', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-
-
-    return image, faces, face_sizes
-
-# plot_face_data generates a histogram of dataset distribution (frequency of face sizes)
-def plot_face_data(face_sizes):
-    if not face_sizes:
-        print("No faces detected in the dataset.")
+# plot_face_data generates a histogram of age distribution
+def plot_age_data(predicted_ages):
+    if not predicted_ages:
+        print("No ages detected in the dataset.")
         return
 
-    mean_size = np.mean(face_sizes)
-    median_size = np.median(face_sizes)
-    min_size = min(face_sizes)
-    max_size = max(face_sizes)
+    # Convert ages to integer for plotting
+    predicted_ages = [int(age) for age in predicted_ages]
 
-    plt.hist(face_sizes, bins=30, color='skyblue', edgecolor='black')
+    # Sort the ages and get unique counts for each age
+    unique_ages, counts = np.unique(predicted_ages, return_counts=True)
 
-    plt.axvline(mean_size, color='red', linestyle='dashed', linewidth=2, label=f'Mean: {mean_size:.2f}')
-    plt.axvline(median_size, color='green', linestyle='dashed', linewidth=2, label=f'Median: {median_size:.2f}')
+    # Plot the histogram
+    plt.figure(figsize=(20, 10))
+    plt.bar(unique_ages, counts, width=0.8)
 
-    plt.title('Distribution of Face Sizes in the Dataset')
-    plt.xlabel('Face Area (pixels²)')
+    plt.xlabel('Age')
     plt.ylabel('Frequency')
-    plt.legend()
+    plt.title('Distribution of Predicted Ages in the Dataset')
 
+    # Rotate the x-axis labels to fit them better
+    plt.xticks(unique_ages, rotation=90)
+
+    plt.tight_layout()
     plt.show()
-
-    # Print basic statistics
-    print(f"Basic Statistics:\nMin Size: {min_size}\nMax Size: {max_size}\nMean Size: {mean_size:.2f}\nMedian Size: {median_size:.2f}")
 
 # process_image takes a path to an image, processes it and show the result and the histogram
 def process_image(file_path):
@@ -118,8 +176,6 @@ def process_image(file_path):
         cv2.destroyAllWindows()
     else:
         print("Error: Image not found")
-
-
 
 
 def augment_image(image):
